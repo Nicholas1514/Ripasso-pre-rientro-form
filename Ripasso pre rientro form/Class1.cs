8304b7d6d19330ac32f59692e3ac34617e36e38b
Nicholas1514 <zappa.nicholas.studente@itispaleocapa.it>
2023-09-08 11:14:42 +0200

    Aggiungere i file di progetto.

---
+++ Ripasso pre rientro form/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ripasso_pre_rientro_form
+{
+    public class Funzioni
+    {
+        public string nfile = @"Zappa.csv";
+        public int ncampi;
+        public int Contacampi(char sep = ';')
+        {
+            
+            string[] linea = File.ReadAllLines(nfile);
+
+            ncampi = linea[0].Split(sep).Length;
+
+            return ncampi;
+        }
+    }
+}
---
+++ Ripasso pre rientro form/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ripasso_pre_rientro_form
+{
+    public partial class Form1 : Form
+    {
+      
+        Funzioni f;
+        public Form1()
+        {
+            InitializeComponent();
+            f = new Funzioni();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int contacampi = f.Contacampi();
+            MessageBox.Show("Il numero dei campi è: " + contacampi);
+        }
+    }
+}
