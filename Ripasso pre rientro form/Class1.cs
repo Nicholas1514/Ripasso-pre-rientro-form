96ed667b6a57013b4f6322d1b1eb8e5ff2f03cdf
Nicholas1514 <zappa.nicholas.studente@itispaleocapa.it>
2023-09-10 17:19:56 +0200

    Funzione Aggiunta campo

--- Ripasso pre rientro form/Class1.cs
+++ Ripasso pre rientro form/Class1.cs
@@ -10,11 +10,20 @@ namespace Ripasso_pre_rientro_form
 {
     public class Funzioni
     {
+        public struct Campi
+        {
+            public string comune;
+            public string provincia;
+            public int anno;
+        }
+
+        public Campi c;
         public string nfile = @"Zappa.csv";
         public int ncampi;
+        public int recordLenght = 64;
         public int Contacampi(char sep = ';')
         {
-            
+
             string[] linea = File.ReadAllLines(nfile);
 
             ncampi = linea[0].Split(sep).Length;
@@ -28,5 +37,89 @@ namespace Ripasso_pre_rientro_form
             sw.WriteLine(contenuto);
             sw.Close();
         }
+
+        public Campi sep(char sep = ';')
+        {
+            Campi c;
+            string[] linee = File.ReadAllLines(nfile);
+
+            string[] campi = linee[1].Split(sep);
+            c.comune = campi[0];
+            c.provincia = campi[1];
+            c.anno = int.Parse(campi[4]);
+            return c;
+        }
+
+        public int Ricerca(string nome, string nfile)
+        {
+
+            int riga = 0;
+            using (StreamReader sr = File.OpenText(nfile))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] dati = s.Split(';');
+
+                    if (dati[0] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+                    if (dati[1] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+                    if (dati[4] == nome)
+                    {
+                        sr.Close();
+                        return riga;
+                    }
+
+                    riga++;
+                }
+                sr.Close();
+            }
+            return -1;
+        }
+
+        public void Aggiuntacampo(string nfile)
+        {
+            Random r = new Random();
+            string[] arr = new string[1000];
+            int riga = 0;
+            using (StreamReader sr = new StreamReader(nfile))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    if (riga == 0)
+                    {
+                        arr[riga] = s + ";miovalore";
+                    }
+                    else
+                    {
+                        string x = (r.Next(10, 21)).ToString();
+                        arr[riga] = s + ";" + x;
+                    }
+                    riga++;
+                   
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(nfile))
+            {
+                riga = 0;
+                while (arr[riga] != null)
+
+                {
+                    sw.WriteLine(arr[riga]);
+                    riga++;
+                }
+            }
+        }
+
+
     }
-}
+    }
+
--- Ripasso pre rientro form/Form1.cs
+++ Ripasso pre rientro form/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -15,11 +16,12 @@ namespace Ripasso_pre_rientro_form
     {
       
         Funzioni f;
+        public string nfile;
         public Form1()
         {
             InitializeComponent();
             f = new Funzioni();
-            
+            nfile = @"Zappa.csv";
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -37,6 +39,33 @@ namespace Ripasso_pre_rientro_form
         {
             f.Aggiuntacoda(textBox1.Text);
             MessageBox.Show("Elemento aggiunto al file");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Funzioni.Campi c = f.sep();
+            MessageBox.Show(c.comune + " " + c.provincia + " " + c.anno);
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int pos = f.Ricerca(textBox2.Text, nfile);
+            if (pos == -1)
+            {
+                MessageBox.Show("Elemento non presente!");
+            }
+            else
+            {
+                MessageBox.Show($"Elemento presente in riga {pos}");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            f.Aggiuntacampo(nfile);
+            MessageBox.Show("E' stato aggiunto il campo");
         }
     }
 }
